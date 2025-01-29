4f8c67d51f76d7826d61372807eb16816569fffd
Rubayat Abdullah <114306964+rubayatabdullah@users.noreply.github.com>
2025-01-29 17:58:20 +0600

    fix some styling issues

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -50,7 +50,7 @@ export default function Calculator(): JSX.Element {
         />
         <button
           type="submit"
-          className="mt-2 w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="mt-5 w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Submit
         </button>
