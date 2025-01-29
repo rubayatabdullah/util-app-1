acb6083c4cc2e477d117f4336cc00d9fae8e3b28
Rubayat Abdullah <114306964+rubayatabdullah@users.noreply.github.com>
2025-01-29 18:15:22 +0600

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Calculator from "./components/Calculator";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Calculator />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-sm text-center p-4 bg-gray-200">
+        <p>&copy; {new Date().getFullYear()} rubayat</p>
+      </footer>
+    </>
+  );
+}
